2faa77eb10ce93cb1822fffba278ef6a24b86cee
Sai Varshith <hosvarshith@gmail.com>
2020-01-04 16:04:20 +0530

    Added some programs

---
+++ 1300_ladder/34B.cpp
@@ -0,0 +1,19 @@
+// http://codeforces.com/problemset/problem/34/B
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n,m;
+	cin >> n >> m;
+	int arr[n];
+	for(int i = 0; i < n; i++){
+		cin >> arr[i];
+	}
+	sort(arr,arr+n);
+	int sum = 0;
+	for(int i = 0; i < m; i++){
+		if (arr[i] < 0)sum += abs(arr[i]);
+	}
+	cout << sum << endl;
+	return 0;
+}
